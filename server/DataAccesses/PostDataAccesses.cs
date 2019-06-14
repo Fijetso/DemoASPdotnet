f6d94db09c3e5a8c15b957e3b72ec4a6ef30cad2
Fijetso <henrynguyen02081997@gmail.com>
2019-06-15 06:34:15 +0700

    Create PostDataAccesses.cs

---
+++ server/DataAccesses/PostDataAccesses.cs
@@ -0,0 +1,23 @@
+using System.Linq;
+using System.Threading.Tasks;
+using server.DataAccesses.Base;
+using server.Models;
+
+namespace server.DataAccesses {
+    public class PostDataAccesses : RealmDatabase {
+        private static int NextId => List.Count () == 0 ?
+            1 : List.AsEnumerable ().Max (i => i.Id) + 1;
+
+        public static IQueryable<Post> List => Database.All<Post> ();
+
+        public static Post Get (int id) => Database.Find<Post> (id);
+        public static async Task<Post> Add (Post post) {
+            await Database.WriteAsync (realm => {
+                post.Id = NextId;
+                post = realm.Add (post);
+            });
+            return post;
+        }
+
+    }
+}
