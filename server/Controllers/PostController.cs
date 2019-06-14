f417d4d86eff656f191339c7dfe3ebecb4d6e5b3
Fijetso <henrynguyen02081997@gmail.com>
2019-06-15 00:16:28 +0700

    Miss some codes

--- server/Controllers/PostController.cs
+++ server/Controllers/PostController.cs
@@ -2,9 +2,6 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace server.Controllers {
     public class PostController : BaseController {
-    [Route ("api/")]
-    [Route ("api/[controller]")]
-    public class PostController : Controller {
         [HttpGet]
         public ActionResult<string> index () {
             return "Hello from Demo ASPDOTNET";
