49dcf9b74329d773916b8fdbfa52641cf38d4a3f
Fijetso <henrynguyen02081997@gmail.com>
2019-06-14 23:57:02 +0700

    Setup default Route with UseMvc()

--- server/Controllers/PostController.cs
+++ server/Controllers/PostController.cs
@@ -1,13 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace server.Controllers
-{
-    [Route("api/[controller]")]
-    public class PostController : Controller
-    {
+namespace server.Controllers {
+    // [Route("api/[controller]")]
+    public class PostController : Controller {
         [HttpGet]
-        public ActionResult<string> index()
-        {
+        public ActionResult<string> index () {
             return "Hello from Demo ASPDOTNET";
         }
     }
