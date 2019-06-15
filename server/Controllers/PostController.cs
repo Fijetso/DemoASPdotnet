cc1cd31a1337637769d235df9e777cdc2449a8fd
Fijetso <henrynguyen02081997@gmail.com>
2019-06-15 08:44:01 +0700

    Update PostContrroller.cs

--- server/Controllers/PostController.cs
+++ server/Controllers/PostController.cs
@@ -1,51 +1,60 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using NSwag.Annotations;
 using Realms;
+using server.Businesses;
 using server.Models;
 
 namespace server.Controllers {
     public class PostController : BaseController {
-        private Realm realm => Realm.GetInstance ();
-        private IQueryable<Post> list => realm.All<Post> ();
-        private int nextId => list.Count () == 0 ?
-            1 : list.AsEnumerable ().Max (p => p.Id) + 1;
+        // private Realm realm => Realm.GetInstance ();
+        // private IQueryable<Post> list => realm.All<Post> ();
+        // private int nextId => list.Count () == 0 ?
+        //     1 : list.AsEnumerable ().Max (p => p.Id) + 1;
 
         /// <summary>
         /// Get A Post List
         /// </summary>
         /// <returns></returns>
         [HttpGet]
-        public ActionResult<IEnumerable<Post>> getAll () => list.ToList ();
+        public ActionResult<List<Post>> getAll () => PostBusiness.List.ToList ();
         /// <summary>
         /// Get Post By I
         /// </summary>
         /// <param name="id">Post ID</param>
         /// <returns></returns>
         [HttpGet ("{int:id}")]
-        public IActionResult GetById (int id) {
-            Post foundPost = realm.Find<Post> (id);
-            if (foundPost == null) {
-                return NotFound ();
-            }
-            return Ok ();
-        }
+        public ActionResult<Post> GetById (int id) => PostBusiness.Get (id);
+        // {
+        //     Post foundPost = realm.Find<Post> (id);
+        //     if (foundPost == null) {
+        //         return NotFound ();
+        //     }
+        //     return Ok ();
+        // }
         /// <summary>
         /// Add new Post
         /// </summary>
         /// <returns></returns>
         [HttpPost]
-        public async Task<ActionResult<Post>> add () {
-            Post p = new Post () {
-                Id = nextId,
-                Title = "My title",
-                Content = "My content",
-                Created = new DateTimeOffset ()
-            };
-            await realm.WriteAsync (r => p = r.Add (p));
-            return p;
+        public async Task<ActionResult<Post>> add ([FromBody] Post post) {
+            post = await PostBusiness.Add (post);
+            return CreatedAtAction (nameof (Post), post);
         }
+        // {
+        //     Post p = new Post () {
+        //         Id = nextId,
+        //         Title = "My title",
+        //         Content = "My content",
+        //         Created = new DateTimeOffset ()
+        //     };
+        //     await realm.WriteAsync (r => p = r.Add (p));
+        //     return p;
+        // }
     }
 }
